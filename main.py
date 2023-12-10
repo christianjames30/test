20ee0d1929deadd6e9738775955e5e28207e0f9d
christianjames30 <153405457+christianjames30@users.noreply.github.com>
2023-12-10 14:37:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
 import uvicorn
-from fastapi import FastAPI
-from typing import Union
-from fastapi.middleware.cors import CORSMiddleware
-app = FastAPI()
-origins = ["*"]
-app.add_middleware( CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
-)
-@app.get("/", tags=["Root"])
-def read_root():
-    return {"Hello": "Yuchen Wu"}
-@app.get("/uni/{uni_id}")
-def read_item(uni_id: str, q: Union[str, None] = None):
-    return {"uni_id":uni_id,"q":q}
+
 if __name__ == "__main__":
-uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
